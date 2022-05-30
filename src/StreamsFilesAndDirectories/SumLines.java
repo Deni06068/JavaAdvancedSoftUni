4eabd55f5c65a10f4359a2dede99704b1dfe9b5f
Deni06068 <Deni06068@gmail.com>
2022-05-30 19:23:28 +0300

    Added new resources.

---
+++ src/StreamsFilesAndDirectories/SumLines.java
@@ -0,0 +1,24 @@
+package StreamsFilesAndDirectories;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SumLines {
+    public static void main(String[] args) {
+        try (BufferedReader bf = new BufferedReader(new FileReader("04. Java-Advanced-Files-and-Streams-Exercises-Resources/input.txt"))){
+            String line = bf.readLine();
+            while (line != null){
+                long sum = 0;
+                char[] charactersFromLine = line.toCharArray();
+                for(char character:charactersFromLine){
+                    sum += character;
+                }
+                line = bf.readLine();
+                System.out.println(sum);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
